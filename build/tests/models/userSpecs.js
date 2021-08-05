0c62bf5fa6bc47cd2de56d6b47465c089daa9a94
humpyreddy96 <82722925+humpyreddy96@users.noreply.github.com>
2021-08-05 13:16:22 -0400

    Delete build directory

--- build/tests/models/userSpecs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_1 = require("../../models/user");
-const store = new user_1.UserHub();
-describe('User Model', () => {
-    it('should have an index method', () => {
-        expect(store.index).toBeDefined();
-    });
-});
